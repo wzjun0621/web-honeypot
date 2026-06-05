f7ae51c0d13710efcd20db451ce31706feec3f17
wzjun0621 <wzjun0621@gmail.com>
2026-06-05 18:49:25 +0900

    Update main.c

--- core/main.c
+++ core/main.c
@@ -2,12 +2,13 @@
 #include "honeypot.h"
 
 int main() {
+    // 서버 시작 메시지
     printf("[*] Starting Web Honeypot Core Module...\n");
     
-    // DB 초기화
+    // DB 초기화 --> attack_logs 테이블 초기화
     logger_init(DB_PATH);
     
-    // 서버 시작 (블로킹)
+    // 서버 시작, 포트 8080에서 소켓 열고 공격자 요청 대기
     server_start(PORT);
     
     return 0;
